8e1dcb226764079b1276caa68e87efa536ef53ab
Leo <qik1992@163.com>
2020-09-08 22:28:50 +0800

    给snowflake添加包名

--- free-im/src/main/java/com/learn/freeim/FreeImApplication.java
+++ free-im/src/main/java/com/learn/freeim/FreeImApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.learn.freeim.dao")
+@MapperScan("com.learn.freeim.mapper")
 public class FreeImApplication {
 
     public static void main(String[] args) {
