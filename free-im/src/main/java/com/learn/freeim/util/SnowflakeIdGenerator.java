7fe956cd51b0b3420809996b11d09fa6cf3212e4
Leo <qik1992@163.com>
2020-09-10 21:26:16 +0800

    添加snowflake配置

--- free-im/src/main/java/com/learn/freeim/util/SnowflakeIdGenerator.java
+++ free-im/src/main/java/com/learn/freeim/util/SnowflakeIdGenerator.java
@@ -1,5 +1,8 @@
 package com.learn.freeim.util;
 
+import com.learn.freeim.config.SnowflakeConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * SnowFlake的结构如下(每部分用-分开):
  * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
@@ -11,67 +14,60 @@ package com.learn.freeim.util;
  * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
  */
 public class SnowflakeIdGenerator{
-    //开始时间截 (2015-01-01)
-    private final long twepoch = 1420041600000L;
+    // 开始时间截 (2015-01-01)
+    private static final long twepoch = 1420041600000L;
 
-    //机器id所占的位数 
-    private final long workerIdBits = 5L;
+    // 机器id所占的位数
+    private static final long workerIdBits = 5L;
 
-    //数据标识id所占的位数 
-    private final long datacenterIdBits = 5L;
+    // 数据标识id所占的位数
+    private static final long datacenterIdBits = 5L;
 
-    //支持的最大机器id，结果是31
-    private final long maxWorkerId = -1L ^ (-1L << workerIdBits);
+    // 支持的最大机器id，结果是31
+    private static final long maxWorkerId = -1L ^ (-1L << workerIdBits);
 
-    //支持的最大数据标识id，结果是31
-    private final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+    // 支持的最大数据标识id，结果是31
+    private static final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
 
-    //序列在id中占的位数
-    private final long sequenceBits = 12L;
+    // 序列在id中占的位数
+    private static final long sequenceBits = 12L;
 
-    //机器ID向左移12位
-    private final long workerIdShift = sequenceBits;
+    // 机器ID向左移12位
+    private static final long workerIdShift = sequenceBits;
 
-    //数据标识id向左移17位(12+5)
-    private final long datacenterIdShift = sequenceBits + workerIdBits;
+    // 数据标识id向左移17位(12+5)
+    private static final long datacenterIdShift = sequenceBits + workerIdBits;
 
-    //时间截向左移22位(5+5+12)
-    private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+    // 时间截向左移22位(5+5+12)
+    private static final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
 
-    //生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
-    private final long sequenceMask = -1L ^ (-1L << sequenceBits);
+    // 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
+    private static final long sequenceMask = -1L ^ (-1L << sequenceBits);
 
-    //工作机器ID(0~31)
-    private long workerId;
+    // 工作机器ID(0~31)
+    private static long workerId;
 
-    //数据中心ID(0~31)
-    private long datacenterId;
+    // 数据中心ID(0~31)
+    private static long datacenterId;
 
-    //毫秒内序列(0~4095)
-    private long sequence = 0L;
+    // 毫秒内序列(0~4095)
+    private static long sequence = 0L;
 
-    //上次生成ID的时间截
-    private long lastTimestamp = -1L;
+    // 上次生成ID的时间截
+    private static long lastTimestamp = -1L;
 
-    /**
-     * @param workerId 工作ID (0~31)
-     * @param datacenterId 数据中心ID (0~31)
-     */
-    public SnowflakeIdGenerator(long workerId, long datacenterId) {
-        if (workerId > maxWorkerId || workerId < 0) {
-            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
-        }
-        if (datacenterId > maxDatacenterId || datacenterId < 0) {
-            throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
-        }
-        this.workerId = workerId;
-        this.datacenterId = datacenterId;
+    @Autowired
+    private SnowflakeConfig snowflakeConfig;
+
+    public SnowflakeIdGenerator() {
+        workerId = snowflakeConfig.getWorkId();
+        datacenterId = snowflakeConfig.getDatacenterId();
     }
 
     /**
      * 获得下一个ID
      */
-    public synchronized long nextId() {
+    public static synchronized long nextId() {
         long timestamp = timeGen();
 
         //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
@@ -107,7 +103,7 @@ public class SnowflakeIdGenerator{
      * @param lastTimestamp 上次生成ID的时间截
      * @return 当前时间戳
      */
-    protected long tilNextMillis(long lastTimestamp) {
+    protected static long tilNextMillis(long lastTimestamp) {
         long timestamp = timeGen();
         while (timestamp <= lastTimestamp) {
             timestamp = timeGen();
@@ -118,7 +114,7 @@ public class SnowflakeIdGenerator{
     /**
      * 返回以毫秒为单位的当前时间
      */
-    protected long timeGen() {
+    protected static long timeGen() {
         return System.currentTimeMillis();
     }
 }
