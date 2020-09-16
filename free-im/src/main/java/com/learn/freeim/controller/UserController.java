22384ad391027260eeecf36f94bc1cf1cb9a3469
LiXiangForJava <1208260820@qq.com>
2020-09-16 20:08:17 +0800

    格式调整

--- free-im/src/main/java/com/learn/freeim/controller/UserController.java
+++ free-im/src/main/java/com/learn/freeim/controller/UserController.java
@@ -27,78 +27,78 @@ import com.learn.freeim.util.VerifyCodeUtil;
 @RequestMapping("user")
 public class UserController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private SnowflakeIdGenerator idGenerator;
+    @Autowired
+    private SnowflakeIdGenerator idGenerator;
 
-	@RequestMapping("register")
-	public Result<String> register(@Validated SysUser sysUser) {
-		userService.usernameHasExist(sysUser);
-		Date now = new Date();
-		sysUser.setCreateDate(now);
-		sysUser.setUpdateDate(now);
-		sysUser.setUserId(idGenerator.nextId());
-		userService.insert(sysUser);
-		return Result.success();
-	}
+    @RequestMapping("register")
+    public Result<String> register(@Validated SysUser sysUser) {
+        userService.usernameHasExist(sysUser);
+        Date now = new Date();
+        sysUser.setCreateDate(now);
+        sysUser.setUpdateDate(now);
+        sysUser.setUserId(idGenerator.nextId());
+        userService.insert(sysUser);
+        return Result.success();
+    }
 
-	@RequestMapping("login")
-	public Result<String> login(SysUser user, HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		String verifyCode = (String) session.getAttribute("verifyCode");
-		session.removeAttribute("verifyCode");
-		if (verifyCode == null || !verifyCode.equals(user.getVerifyCode())) {
-			throw new CommonException("1004", LOGGER);
-		}
-		SysUser sourceUser = userService.queryUserByUsername(user);
-		if (sourceUser == null) {
-			throw new CommonException("1005", LOGGER);
-		}
-		if (sourceUser.getUserName().equals(user.getUserName())
-		        && sourceUser.getPassword().equals(user.getPassword())) {
-			return Result.success();
-		}
-		throw new CommonException("1006", LOGGER);
-	}
+    @RequestMapping("login")
+    public Result<String> login(SysUser user, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String verifyCode = (String) session.getAttribute("verifyCode");
+        session.removeAttribute("verifyCode");
+        if (verifyCode == null || !verifyCode.equals(user.getVerifyCode())) {
+            throw new CommonException("1004", LOGGER);
+        }
+        SysUser sourceUser = userService.queryUserByUsername(user);
+        if (sourceUser == null) {
+            throw new CommonException("1005", LOGGER);
+        }
+        if (sourceUser.getUserName().equals(user.getUserName())
+                && sourceUser.getPassword().equals(user.getPassword())) {
+            return Result.success();
+        }
+        throw new CommonException("1006", LOGGER);
+    }
 
-	/*
-	 * 获取验证码
-	 */
-	@RequestMapping(value = "/verifyCode")
-	public void getVerifyCode(HttpServletResponse response, HttpServletRequest request) {
-		ServletOutputStream outputStream = null;
-		try {
-			VerifyImageCode imageCode = VerifyCodeUtil.createVerifyImageCode();
+    /*
+     * 获取验证码
+     */
+    @RequestMapping(value = "/verifyCode")
+    public void getVerifyCode(HttpServletResponse response, HttpServletRequest request) {
+        ServletOutputStream outputStream = null;
+        try {
+            VerifyImageCode imageCode = VerifyCodeUtil.createVerifyImageCode();
 
-			// 将验证码存到session
-			HttpSession session = request.getSession();
-			session.setAttribute("verifyCode", imageCode.getVerifyCode());
-			LOGGER.info("后台生成的用户登录验证码：{}", imageCode.getVerifyCode());
-			// 禁止图像缓存
-			response.setHeader("Pragma", "no-cache");
-			response.setHeader("Cache-Control", "no-cache");
-			response.setDateHeader("Expires", 0);
-			response.setContentType("image/jpeg");
-			// 将图像输出到Servlet输出流中。
-			outputStream = response.getOutputStream();
-			ImageIO.write(imageCode.getImage(), "jpeg", outputStream);
+            // 将验证码存到session
+            HttpSession session = request.getSession();
+            session.setAttribute("verifyCode", imageCode.getVerifyCode());
+            LOGGER.info("后台生成的用户登录验证码：{}", imageCode.getVerifyCode());
+            // 禁止图像缓存
+            response.setHeader("Pragma", "no-cache");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setContentType("image/jpeg");
+            // 将图像输出到Servlet输出流中。
+            outputStream = response.getOutputStream();
+            ImageIO.write(imageCode.getImage(), "jpeg", outputStream);
 
-		} catch (Exception e) {
-			LOGGER.error("获取验证码出错", e);
-		} finally {
-			try {
-				if (outputStream != null) {
-					outputStream.close();
-				}
-			} catch (Exception e) {
-				LOGGER.error("关闭流出错", e);
-			}
-		}
+        } catch (Exception e) {
+            LOGGER.error("获取验证码出错", e);
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            } catch (Exception e) {
+                LOGGER.error("关闭流出错", e);
+            }
+        }
 
-	}
+    }
 
 }
